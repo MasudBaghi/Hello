53f485b4234ad399f44a32ab6ee6804db295dc4d
Masudkurd <139621559+Masudkurd@users.noreply.github.com>
2023-07-15 18:00:37 +0330

    Add files via upload

---
+++ slp3.py
@@ -0,0 +1,14 @@
+import streamlit as st
+st.write('''
+
+ in the name of god
+this nomber one **app** **masud!* * 
+
+
+''')
+
+st.header('Helo Soltan')
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+    st.write('ali najand**khobi**')
